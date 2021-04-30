e078773ab5b350c0bb360f20b9fd452fc9795c3c
Michael Ryan <michaelryan2895@gmail.com>
2021-04-30 01:00:25 -0400

    connecting front end to back end

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 
 class BestBooks extends React.Component{
+  
+  componentDidMount = async() => {
+    const getBookData = await axios.get(`${process.env.REACT_APP_DATABASE_URL}/books?user='michaelryan2895@gmail.com'`);
+    console.log(getBookData.data);
+
+
+  }
+
   render() {
     return(
-      
+      <h1>Books</h1>
     )
   }
 }
+
+export default BestBooks;
